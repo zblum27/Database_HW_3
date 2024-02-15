5dc2ff32bb96d95d7a9ba386ec0de7fdcc3f3c16
zblum27 <blum.z@northeastern.edu>
2024-02-15 13:22:12 -0800

    QUERY2 FIX

--- Query2.sql
+++ Query2.sql
@@ -1,12 +1,6 @@
 --Write the queries that insert at least 10 videos, respecting the problem rules.
-INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES
-    (1, 'Director 1'),
-    (2, 'Director 2'),
-    (3, 'Director 3'),
-    (4, 'Director 4'),
-    (5, 'Director 5'),
-    (6, 'Director 6'),
-    (7, 'Director 7'),
-    (8, 'Director 8'),
-    (9, 'Director 9'),
-    (10, 'Director 10');
+INSERT INTO MusicVideo (musicVideo, VideoDirector)
+ SELECT DISTINCT Trackid AS musicVideo_id, "director Name"
+ FROM tracks
+ LIMIT
+ 10;
