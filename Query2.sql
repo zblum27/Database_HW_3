27f7e617aa63124f342bd06f3dc50e645688f41f
zblum27 <144292627+zblum27@users.noreply.github.com>
2024-02-14 19:16:00 -0800

    Add files via upload

---
+++ Query2.sql
@@ -0,0 +1,12 @@
+--Write the queries that insert at least 10 videos, respecting the problem rules.
+INSERT INTO MusicVideo (TrackId, VideoDirector) VALUES
+    (1, 'Director 1'),
+    (2, 'Director 2'),
+    (3, 'Director 3'),
+    (4, 'Director 4'),
+    (5, 'Director 5'),
+    (6, 'Director 6'),
+    (7, 'Director 7'),
+    (8, 'Director 8'),
+    (9, 'Director 9'),
+    (10, 'Director 10');
